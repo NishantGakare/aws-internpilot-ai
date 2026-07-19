b93b9a5b95542aa9e483120d9912811420216441
NishantGakare <gakarearyan@gmail.com>
2026-07-20 00:05:59 +0530

    Phase 3

--- backend/internpilot-backend/resume_processor/app.py
+++ backend/internpilot-backend/resume_processor/app.py
@@ -1,5 +1,9 @@
+import json
+
 def lambda_handler(event, context):
-    print(event)
+
+    print("========== S3 EVENT ==========")
+    print(json.dumps(event, indent=2))
 
     return {
         "statusCode": 200
